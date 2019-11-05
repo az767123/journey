5209bf3d7b779861dc99566e465cc6c54c38fca9
unknown <767144054@qq.com>
2019-11-05 16:54:15 +0800

    init

---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>首页
+    <home-header></home-header>
+  </div>
+
+</template>
+
+<script>
+
+  import homeHeader from "./homeheader";
+  export default {
+    name: "home",
+    components:{
+      homeHeader
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @color:red;
+  div {
+    color:@color;
+  }
+
+</style>
